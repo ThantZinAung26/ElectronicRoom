48d6e0f5b7aac700d309b8e61e5e41adf069217d
ThantZinAung26 <thantzinblogger@gmail.com>
2019-06-03 23:58:39 +0630

    update

--- app/src/main/java/com/soft/electronicroom/CategoryFragment.java
+++ app/src/main/java/com/soft/electronicroom/CategoryFragment.java
@@ -1,21 +1,15 @@
 package com.soft.electronicroom;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.soft.electronicroom.adapter.CategoryAdapter;
@@ -43,7 +37,7 @@ public class CategoryFragment extends Fragment {
                 .mainCategoryDAO());
 
 
-        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView = view.findViewById(R.id.recycler_view);
 //        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
 //      recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.HORIZONTAL));
